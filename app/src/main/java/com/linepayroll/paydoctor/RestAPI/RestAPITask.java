4eaca76426afc66ae9aa0b5b86743fb4836ae7f9
eisen <eisen1990@naver.com>
2017-11-13 16:13:41 +0900

    RestAPITask Class 주석 추가

--- app/src/main/java/com/linepayroll/paydoctor/RestAPI/RestAPITask.java
+++ app/src/main/java/com/linepayroll/paydoctor/RestAPI/RestAPITask.java
@@ -14,9 +14,23 @@ import java.net.URL;
 
 /**
  * Created by eisen on 2017-11-13.
+ *
+ * FIXME : 해당 클래스는 RestAPI 호출을 위한 Sample Source Code이므로,
+ * FIXME:  각각의 Activity들에서 Rest API 호출이 필요할 경우 아래 코드를 참고하여
+ * FIXME : Parameter들에 맞게 작성할 것.
+ * Rest API는 모두 JSON 방식으로 Return 하므로, AsyncTask<String, Void, JSONObject>의 JSONObject는 기본적으로 동일하다.
+ * String 인자는 execute()의 Parameter로, 다수의 String type veriable이 올 수 있다. 그리고 execute()의 Parameter는
+ * doInBackground(String... params)에 전달되며, execute(첫 번째, 두 번째, n번 째) Argument는 doInBackground의 params에서 params[0], params[1], params[n-1]
+ *
+ * Sample Code:
+ *      ... source code ...
+ *
+ *      JSONObject ResultObject = new RestAPITask().execute(SOME_URL_STRING_VALUE);
+ *
+ *      ... source code ...
  */
 
-public class RestAPITask extends AsyncTask <String, Void, JSONObject>{
+public class RestAPITask extends AsyncTask<String, Void, JSONObject> {
 
     public RestAPITask() {
         super();
@@ -69,17 +83,21 @@ public class RestAPITask extends AsyncTask <String, Void, JSONObject>{
             BufferedReader Reader = new BufferedReader(InputStream);
             StringBuilder Builder = new StringBuilder();
             String ResultStr;
-            while( ( ResultStr = Reader.readLine() ) != null ) {
+
+            while ((ResultStr = Reader.readLine()) != null) {
                 Builder.append(ResultStr + "\n");
             }
 
             result = new JSONObject(Builder.toString());
 
-        } catch (MalformedURLException e){
+        } catch (MalformedURLException e) {
             e.printStackTrace();
+            result = null;
         } catch (Exception e) {
             e.printStackTrace();
+            result = null;
         } finally {
+            //Todo finally..
         }
 
         return result;
